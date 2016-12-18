74f079746fbe722d8f3bb0294ae61786b71657d9
Silvio Netto <silvio.netto@gmail.com>
2016-12-18 21:15:07 +0100

    Adding review

--- app.js
+++ app.js
@@ -17,6 +17,15 @@
 				};
 			});
 
+			app.controller('ReviewController', function(){
+				this.review = {};
+
+				this.addReview = function(product) {
+					product.reviews.push(this.review);
+					this.review = {};
+				};
+			});
+
 			var gems = [
 				{
 					name: 'Dodecahedron',
